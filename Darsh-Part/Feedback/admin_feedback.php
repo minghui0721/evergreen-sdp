91483349e5087ca6660fe682f23fdb733bd39361
iidarshinii <127184239+iidarshinii@users.noreply.github.com>
2023-08-05 12:49:23 +0800

    Add files via upload

---
+++ Darsh-Part/Feedback/admin_feedback.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Admin - Student Feedback Responses</title>
+  <link rel="stylesheet" type="text/css" href="admin_feedback.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cooper+Black&display=swap">
+</head>
+<body>
+  <div class = "wrapper">
+  <div class="container">
+    <table>
+    <h1>Student Feedback Responses</h1>
+      <tr>
+        <th>Rating</th>
+        <th>Issues</th>
+        <th>Suggestions</th>
+        <th>Action</th>
+      </tr>
+      <?php
+        // Establish database connection
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $database = 'sdp';
+        $connection = mysqli_connect($host, $user, $password, $database);
+
+        // Check connection
+        if (!$connection) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Handle delete operation if "delete" parameter is passed in the URL
+        if (isset($_GET['delete'])) {
+          $delete_id = $_GET['delete'];
+          $stmt = $connection->prepare("DELETE FROM feedback WHERE id = ?");
+          $stmt->bind_param("i", $delete_id);
+          $stmt->execute();
+          $stmt->close();
+          header("Location: admin_feedback.php");
+          exit();
+        }
+
+        // Fetch feedback responses from the database
+        $sql = "SELECT * FROM feedback";
+        $result = $connection->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["rating"] . "</td>";
+                echo "<td>" . $row["issues"] . "</td>";
+                echo "<td>" . $row["suggestions"] . "</td>";
+                echo "<td><a href='admin_feedback.php?delete=" . $row["id"] . "'>Delete</a></td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "<tr><td colspan='5'>No feedback responses yet.</td></tr>";
+        }
+
+        // Close connection
+        $connection->close();
+      ?>
+    </table>
+  </div>
+  </div>
+</body>
+</html>
+
+
